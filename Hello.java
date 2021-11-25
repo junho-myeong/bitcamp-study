18d8a9152c809714a93fd8db101b10f5ac6602c4
junho-myeong <https://github.com/junho-myeong?tab=repositories>
2021-11-25 20:34:24 +0900

    'test'

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello {
   public static void main(String[] args) {
+    System.out.println("dkkdkd");
     System.out.println("Hello, world!1");
     System.out.println("Hello, world!2");
     System.out.println("Hello, world!3");
