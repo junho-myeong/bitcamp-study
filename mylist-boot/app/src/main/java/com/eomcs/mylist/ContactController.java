458d69227f943ce04fd8e6d729706f4dd7a5676f
junho-myeong <wnsgh9166@naver.com>
2021-12-13 18:00:55 +0900

    day26

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -8,21 +8,65 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class ContactController {
 
+  String[] contacts = new String[5];
+  int size = 0;
+  int updateIdx;
+
   @RequestMapping("/contact/list") // 이 URL(unified resource locator)이 연결되면 이 메소드를 호출할것이다.
   public Object list() {
-    String[] contacts = {
-        "aaa11,aaa@test.com,1111,비트캠프",
-        "aaa22,aaa@test.com,1111,비트캠프",
-        "aaa33,aaa@test.com,1111,비트캠프",
-        "aaa44,aaa@test.com,1111,비트캠프",
-        "aaa55,aaa@test.com,1111,비트캠프"
-    };
-    return contacts;
+    //    String[] contacts = {
+    //        "aaa11,aaa@test.com,1111,비트캠프",
+    //        "aaa22,aaa@test.com,1111,비트캠프",
+    //        "aaa33,aaa@test.com,1111,비트캠프",
+    //        "aaa44,aaa@test.com,1111,비트캠프",
+    //        "aaa55,aaa@test.com,1111,비트캠프"
+    //    };
+    String[] arr = new String[size]; // 배열에 저장된 값만 복사할 새 배열을 만든다.
+    for (int i = 0; i<size;i++) {
+      arr[i] = contacts[i]; // 전체 배열에서 값이 들어 있는 항목만 복사한다.
+    }
+    return arr; // 복사한 배열 항목들을 담고 있는 새 배열을 리턴한다.
   }
 
   @RequestMapping("/contact/add")
   public Object add(String name, String email, String tel, String company) {
     String contact = name + "," + email + "," + tel + "," + company;
-    return contact;
+    contacts[size++] = contact;
+    return size; //현재 몇개를 입력했는지 알기위해 
+  }
+
+  @RequestMapping("/contact/get")
+  public Object get(String email) {
+    for (int i = 0; i<size;i++) {
+      if(contacts[i].split(",")[1].equals(email)) {
+        updateIdx = i;
+        return contacts[i];
+      }
+    }
+    return "";
+  }
+  @RequestMapping("/contact/update")
+  public Object update(String name, String email, String tel, String company) {
+    contacts[updateIdx] = name + "," + email + "," + tel + "," + company;
+    return updateIdx;
+  }
+  @RequestMapping("/contact/delete")
+  public Object delete(String email) {
+    for (int i = 0; i<size;i++) {
+      if(contacts[i].split(",")[1].equals(email)) {
+
+        // 현재 위치의 다음 항목에서 배열 끝까지 반복하면 앞으로 갚을 당겨온다.
+        for (int j = i +1;j<size ; j++) {
+          contacts[j-1] = contacts[j];
+        }
+        size--;
+        return 1;
+      }
+    }
+    return 0;
   }
 }
+
+
+
+
