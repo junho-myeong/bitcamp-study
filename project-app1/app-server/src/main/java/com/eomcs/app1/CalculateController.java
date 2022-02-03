5d94155a71052f8d30701388cdab147da5d2f6dc
junho-myeong <wnsgh9166@naver.com>
2022-02-03 16:24:09 +0900

    .

--- project-app1/app-server/src/main/java/com/eomcs/app1/CalculateController.java
+++ project-app1/app-server/src/main/java/com/eomcs/app1/CalculateController.java
@@ -5,26 +5,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculateController {
+
   @RequestMapping("/calc")
-  // 쿼리 스트링에 변수와 값을 받는 방법
-  // URL Decoding 할필요도 없다.
   public String calc(String op, double a, double b) {
     String response = null;
+
     switch (op) {
       case "+": 
-        response = String.format("천재명준호가 보냄!%.1f + %.1f  = %.2f", a,  b, (a+b));
+        response = String.format("%.2f", a + b);
         break;
       case "-": 
-        response = String.format("천재명준호가 보냄!%.1f - %.1f  = %.2f", a,  b, (a-b));
+        response = String.format("%.2f", a - b);
         break;
-      case "*":
-        response = String.format("천재명준호가 보냄!%.1f  * %.1f  = %.2f", a,  b, (a*b));
+      case "*": 
+        response = String.format("%.2f", a * b);
+        break;  
+      case "/": 
+        response = String.format("%.2f", a / b);
         break;
-      case "/":
-        response = String.format("천재명준호가 보냄!%.1f / %.1f  = %.2f", a,  b, (a/b));
-        break;
-      default:
-        response = "명준호: 지원하지 않는 연산자 입니다.";
+      default: 
+        response = "연산 불가!";
     }
     return response;
   }
--- project-app1/app-server/src/main/java/com/eomcs/app1/ServerApp.java
+++ project-app1/app-server/src/main/java/com/eomcs/app1/ServerApp.java
@@ -19,14 +19,12 @@ public class ServerApp {
     strBuilder.append("<!DOCTYPE html>\n");
     strBuilder.append("<html><head><meta charset='UTF-8'><title>웹계산기</title></head>\n");
     strBuilder.append("<body>\n");
-    strBuilder.append("<h1>[웹 계산기 도움말]</h1>\n");
-    strBuilder.append("사용법:<br> \n");
-    strBuilder.append("URL 형식 => http://서버주소:포트번호/clac?op=연산자&a=값1&b=값2<br>\n");
-    strBuilder.append("예) http://localhost:8080/clac?op=-&a=100&b=200<br>\n");
+    strBuilder.append("<h1>웹 계산기 도움말</h1>\n");
+    strBuilder.append("사용법:<br>\n");
+    strBuilder.append("URL 형식 => http://서버주소:포트번호/calc?op=연산자&a=값1&b=값2<br>\n");
+    strBuilder.append("예) http://localhost:8080/calc?op=-&a=100&b=200<br>\n");
     strBuilder.append("</body></html>\n");
-    // \n 은 html에는 적용이 안되지만 웹브라우저에서 페이스 소스 보기에서 쉽게 보기 위해 사용하는것이다.
+
     return strBuilder.toString();
   }
 }
-
-
