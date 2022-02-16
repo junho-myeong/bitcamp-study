b7d6df253452886ae2c4619258f73f69ab70ba75
junho-myeong <wnsgh9166@naver.com>
2022-02-16 11:53:25 +0900

    .

--- project-app2/app/src/main/java/com/eomcs/app2/App.java
+++ project-app2/app/src/main/java/com/eomcs/app2/App.java
@@ -1,41 +1,83 @@
-
+// promt함수 생성하는거 다시 확인해보기
 package com.eomcs.app2;
 
 import java.util.Scanner;
 
 public class App {
+  Scanner keyScan = new Scanner(System.in);
   public static void main(String[] args) {
-    Scanner keyScan = new Scanner(System.in);
+    new App().service();
+
+
+  }
+  public void service() {
 
     while (true) {
-      System.out.println("메뉴:");
-      System.out.println("1. 등록");
-      System.out.println("2. 목록");
-      System.out.println("3. 상세");
-      System.out.println("4. 변경");
-      System.out.println("5. 삭제");      
-      System.out.print("명령>");
-      String input = keyScan.nextLine();
-      if (input.equals("quit") || input.equals("exit")) {
+      printMenu();
+      String input = promptString("명령>");
+      if (checkQuit(input)) {
         break;
       }
-      switch (input) {
-        case "1":
-          break;
-        case "2":
-          break;
-        case "3":
-          break;
-        case "4":
-          break;
-        case "5":
-          break;
-        default:
-          System.out.println("올바른 메뉴 번호를 입력하시오");
+      try {
+        switch (input) {
+          case "1": createScore(); break;
+          case "2": listScore(); break;
+          case "3": detailScore(); break;
+          case "4": updateScore(); break;
+          case "5": deleteScore(); break;
+          default:
+            System.out.println("올바른 메뉴 번호를 입력하시오");
+        }
+      } catch(Exception e) {
+        System.out.println("실행중 오류 발생: " + e.getMessage());
       }
+      System.out.println();
     }
 
     System.out.println("종료!");
     keyScan.close();
   }
+
+  private void printMenu() {
+    System.out.println("메뉴:");
+    System.out.println("1. 등록");
+    System.out.println("2. 목록");
+    System.out.println("3. 상세");
+    System.out.println("4. 변경");
+    System.out.println("5. 삭제");
+  }
+
+  private String promptString(String title) {
+    System.out.print(title);
+    return keyScan.nextLine();
+  }
+
+  private int promptInt(String title) {
+    return Integer.parseInt(promptString(title));
+  }
+
+  private boolean checkQuit(String input) {
+    return input.equals("quit") || input.equals("exit");
+  }
+
+  private void createScore() {
+    Score score = new Score();
+    score.setName(promptString("이름?"));
+    score.setKor(promptInt("국어?"));
+    score.setEng(promptInt("영어?"));
+    score.setMath(promptInt("수학?"));
+  }
+  private void listScore() {
+
+  }
+  private void detailScore() {
+
+  }
+  private void updateScore() {
+
+  }
+  private void deleteScore() {
+
+  }
+
 }
