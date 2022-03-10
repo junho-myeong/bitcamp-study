ba835149765047e8ab1ec2687327fc2d9422a054
junho-myeong <wnsgh9166@naver.com>
2022-03-10 12:35:50 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BoardController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/controller/BoardController.java
@@ -50,8 +50,8 @@ public class BoardController {
   }
 
   @RequestMapping("/board/delete")
-  public Object delete(int index) throws Exception {
-    return boardDao.delete(index);
+  public Object delete(int no) throws Exception {
+    return boardDao.delete(no);
   }
 }
 
