808af6106e17c867626c8f1b0e3302747b382b7c
junho-myeong <wnsgh9166@naver.com>
2022-03-11 14:16:53 +0900

    /

--- mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/App.java
@@ -1,6 +1,7 @@
 package com.eomcs.mylist;
 
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,7 +21,7 @@ public class App {
 
   // DB 커넥션풀 준비
   // 모든 커넥션 풀은 DataSource 인터페이스 규칙에 따라 만들어진다.
-  public static DataSource dataSource; // DataSource는 인터페이스이다 그 밑에 자식들을 언제든지 변수에 받을수잇다.
+  // public static DataSource dataSource; // DataSource는 인터페이스이다 그 밑에 자식들을 언제든지 변수에 받을수잇다.
 
   //  static {
   //    try { // 여기서 try리소스 하지 않는 이유는 DAO에서 사용하기 위해 만드는것이지 
@@ -33,17 +34,37 @@ public class App {
   //    }
   //  }
 
+  // @Value 애노테이션은 application.properties에 있는 키의 값을 자동으로 주입하는것이다
+  // 이값을 인스턴스 필드로 받을수 잇지만 파라미터로 받을수도 잇다
+  // application.properties 사용 이유 ?
+  // 객체 이름이나 DBMS가 바뀌어도 한곳에서만 바꾸면 되기 때문이다.
+  //
+  //  @Value("${spring.datasource.driver-class-name}")
+  //  String driverClassName;
+  //
+  //  @Value("${spring.datasource.url}")
+  //  String url;
+  //
+  //  @Value("${spring.datasource.username}")
+  //  String userName;
+  //
+  //  @Value("${spring.datasource.password}")
+  //  String pwd;
 
   // Spring 프레임 워크(IoC = 객체풀)에서 객체를 생성한 후 보관하도록 만드는 방법
-  @Bean // => 스프링 프레임워크에게 다음 메서드를 호출한 후 이 메서드가 리턴하는 값을 스프링 부트에 보관하라고 지시한다.
-  public DataSource createDataSource() {
-    System.out.println("크리에이트 데이타 소스 호출됨!");
+  @Bean // => 스프링 부트를 시작할 때 다음 메서드를 호출하게 만든다.
+  // => 이 메서드가 리턴한 값은 스프링 부트의 객체풀(Object pool)에 보관한다.
+  public DataSource dataSource(@Value("${spring.datasource.driver-class-name}") String driverClassName,
+      @Value("${spring.datasource.url}") String url,
+      @Value("${spring.datasource.username}") String userName,
+      @Value("${spring.datasource.password}") String pwd) {
+
     try {
       DriverManagerDataSource connectionPool =new DriverManagerDataSource(); // DB커넥션 풀 준비
-      connectionPool.setDriverClassName("org.mariadb.jdbc.Driver"); // 커넥션 풀에서 어떤 DB를 사용할것인지
-      connectionPool.setUrl("jdbc:mariadb://localhost:3306/studydb"); // 커넥션 풀에서 어떤 주소로 찾아 갈것인지
-      connectionPool.setUsername("study"); 
-      connectionPool.setPassword("1111");
+      connectionPool.setDriverClassName(driverClassName); // 커넥션 풀에서 어떤 DB를 사용할것인지
+      connectionPool.setUrl(url); // 커넥션 풀에서 어떤 주소로 찾아 갈것인지
+      connectionPool.setUsername(userName); 
+      connectionPool.setPassword(pwd);
 
       return connectionPool;
     } catch (Exception e) {
