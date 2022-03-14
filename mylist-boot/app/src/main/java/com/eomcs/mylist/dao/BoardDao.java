1942542a33281810ffa9893eb07a736ce8d5b14c
junho-myeong <wnsgh9166@naver.com>
2022-03-14 15:03:53 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/BoardDao.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/BoardDao.java
@@ -2,8 +2,16 @@
 package com.eomcs.mylist.dao;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
 import com.eomcs.mylist.domain.Board;
 
+@Mapper 
+// => Mybatis에서 DAO구현체를 자동으로 생성한다.
+// => DAO구현체가 사용할 SQL Mapper 파일의 위치는 인터페이스 패키지 경로및 이름이 일치해야한다.
+//      예) com/eomcs/mylist/dao/BoardDao.xml
+// => 인터페이스에 메서드를 호출될때 사용할 SQL ID는 메서드 이름과 일치해야한다.
+// 예) <select id="countAll">...</select>
+//
 public interface BoardDao {
 
   // 인터페이스는 객체의 메서드 호출 규칙을 정의하는 것이기 때문에
