3731a14f8e9296e28a5686472ad7457890e89b30
junho-myeong <wnsgh9166@naver.com>
2022-03-23 15:28:21 +0900

    .

--- web/app/src/main/java/com/eomcs/web/html/FormController.java
+++ web/app/src/main/java/com/eomcs/web/html/FormController.java
@@ -1,12 +1,18 @@
 package com.eomcs.web.html;
 
+import java.io.File;
 import java.util.HashMap;
+import javax.servlet.ServletContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class FormController {
+
+  @Autowired
+  ServletContext sc; // 서버에 배포된 웹 애플리케이션에 정보를 다루는 객체
 
   @RequestMapping("/html/form/exam01")
   public Object exam01() {
@@ -27,6 +33,32 @@ public class FormController {
     System.out.println(name);
     System.out.println(age);
     System.out.println(photo.getOriginalFilename());
+
+    try {
+      File photoFile = new File("c:/upload/" + photo.getOriginalFilename());
+      photo.transferTo(photoFile);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return "error";
+    }
+
+    return "ok"; 
+  }
+
+  @RequestMapping("/html/form/exam12")
+  public Object exam04(String name, int age, MultipartFile[] photo) {
+    System.out.println(name);
+    System.out.println(age);
+
+    for (MultipartFile part : photo) {
+      try {
+        File photoFile = new File("c:/upload/" + part.getOriginalFilename());
+        part.transferTo(photoFile);
+      } catch (Exception e) {
+        e.printStackTrace();
+        return "error";
+      }
+    }
     return "ok"; 
   }
 }
